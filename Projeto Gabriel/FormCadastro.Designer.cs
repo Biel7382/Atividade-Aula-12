c0e5d1ed2314dd3084262debc462317dc5843fc3
Thiago Attico <thiagoattico@hotmail.com>
2024-11-13 23:29:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Gabriel/FormCadastro.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Projeto_Gabriel
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNome = new TextBox();
+            txtPreco = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnSalvar = new Button();
+            SuspendLayout();
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(159, 57);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(125, 27);
+            txtNome.TabIndex = 0;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(159, 90);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(125, 27);
+            txtPreco.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(87, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(87, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Preço";
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(135, 148);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(94, 29);
+            btnSalvar.TabIndex = 4;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // FormCadastro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(392, 236);
+            Controls.Add(btnSalvar);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNome);
+            Name = "FormCadastro";
+            Text = "FormCadastro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private TextBox txtPreco;
+        private Label label1;
+        private Label label2;
+        private Button btnSalvar;
+    }
+}
